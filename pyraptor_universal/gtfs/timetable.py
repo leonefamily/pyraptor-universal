cb116a7b88fbf0d5bc80282e92c6cb80fbfb4413
leonefamily <leonefamily@seznam.cz>
2023-09-15 23:23:54 +0200

    Fixed date picking

--- pyraptor_universal/gtfs/timetable.py
+++ pyraptor_universal/gtfs/timetable.py
@@ -6,6 +6,7 @@ from typing import List
 from dataclasses import dataclass
 from collections import defaultdict
 from datetime import datetime as dt
+from pathlib import Path
 
 import pandas as pd
 from loguru import logger
@@ -67,62 +68,6 @@ def parse_arguments(args_from: list = sys.argv[1:]):
     return arguments
 
 
-def get_trips(
-    input_folder: str,
-    departure_date: str = None,
-) -> pd.DataFrame:
-
-    date_dt = None if departure_date is None else dt.strptime(departure_date, '%Y%m%d')
-
-    trips = pd.read_csv(input_folder + '/trips.txt')
-    calendar = pd.read_csv(input_folder + '/calendar.txt')
-    calendar.start_date = pd.to_datetime(calendar.start_date, format='%Y%m%d')
-    calendar.end_date = pd.to_datetime(calendar.end_date, format='%Y%m%d')
-
-    cdpath = input_folder + '/calendar_dates.txt'
-    cdexist = os.path.exists(cdpath)
-    if cdexist:
-        cdates = pd.read_csv(cdpath)
-        cdates.date = pd.to_datetime(cdates.date, format='%Y%m%d').tolist()
-
-    mostday = defaultdict(int)
-    servday = defaultdict(list)
-    for service_id, service_df in trips.groupby('service_id'):
-        cal_df = calendar[calendar['service_id'] == service_id]
-        sd, ed = cal_df[['start_date', 'end_date']].iloc[0]
-        allowed_wdays = cal_df[WEEKDAYS + WEEKEND].iloc[0].to_dict()
-        
-        if date_dt is not None:
-            sdd = pd.to_datetime(max(sd, date_dt))
-            edd = pd.to_datetime(min(ed, date_dt))
-            drange = pd.date_range(sdd, edd)
-        else:
-            drange = pd.date_range(sd, ed)
-        allowed_days = []
-
-        for awd in drange:
-            d_name = awd.day_name().lower()
-            if cdexist:
-                excluded = bool(
-                    len(cdates[(cdates['date'].dt.date == awd.date()) &
-                               (cdates['service_id'] == service_id) &
-                               (cdates['exception_type'] == 2)])
-                    )
-            else:
-                excluded = False
-            if allowed_wdays[d_name] and not excluded:
-                allowed_days.append(awd)
-
-        for ald in allowed_days:
-            mostday[str(ald.date())] += len(service_df)
-            servday[str(ald.date())].append(service_id)
-        # print(service_id)
-
-    mk = max(mostday, key=mostday.get)
-    trips = trips[trips['service_id'].isin(servday[mk])]
-    return trips
-
-
 def main(
     input_folder: str,
     output_folder: str,
@@ -136,6 +81,35 @@ def main(
     gtfs_timetable = read_gtfs_timetable(input_folder, departure_date)
     timetable = gtfs_to_pyraptor2_timetable(gtfs_timetable)
     write_timetable(output_folder, timetable)
+
+
+def filter_trips_by_date(
+        trips: pd.DataFrame,
+        calendar: pd.DataFrame,
+        departure_date: dt,
+        calendar_dates: pd.DataFrame = None
+) -> pd.DataFrame:
+    servday = defaultdict(list)
+    for service_id, service_df in trips.groupby('service_id'):
+        cal_df = calendar[calendar['service_id'] == service_id]
+        sd, ed = cal_df[['start_date', 'end_date']].iloc[0]
+        allowed_wdays = cal_df[WEEKDAYS + WEEKEND].iloc[0].to_dict()
+        drange = pd.date_range(sd, ed)
+        allowed_days = []
+
+        for awd in drange:
+            d_name = awd.day_name().lower()
+            excluded = bool(len(calendar_dates[(calendar_dates['date'].dt.date == awd.date()) &
+                                       (calendar_dates['service_id'] == service_id) &
+                                       (calendar_dates['exception_type'] == 2)]))
+            if allowed_wdays[d_name] and not excluded:
+                allowed_days.append(awd)
+
+        for ald in allowed_days:
+            servday[str(ald.date())].append(service_id)
+
+    trips = trips[trips['service_id'].isin(servday[departure_date.strftime('%Y-%m-%d')])]
+    return trips
 
 
 def read_gtfs_timetable(
@@ -156,7 +130,6 @@ def read_gtfs_timetable(
     # Read trips
     logger.debug("Read Trips")
 
-    # trips = get_trips(input_folder, departure_date)
     trips = pd.read_csv(os.path.join(input_folder, "trips.txt"))
     trips = trips.merge(routes[['route_id', 'route_short_name', 'route_type']])
     trips = trips[
@@ -166,18 +139,28 @@ def read_gtfs_timetable(
             "trip_id",
             "route_short_name",
             "route_type"
-            # "trip_short_name",
-            # "trip_long_name",
         ]
     ]
-    # trips["trip_short_name"] = trips["trip_short_name"].astype("Int64")
 
     calendar = pd.read_csv(
-        os.path.join(input_folder, "calendar_dates.txt"), dtype={"date": str}
+        os.path.join(input_folder, "calendar.txt"), dtype={"start_date": str, "end_date": str}
     )
-    calendar = calendar[calendar.service_id.isin(trips.service_id.values)]
-    trips = trips.merge(calendar[["service_id", "date"]], on="service_id")
-    trips = trips[trips.date == departure_date]
+    calendar['start_date'] = pd.to_datetime(calendar['start_date'], format='%Y%m%d')
+    calendar['end_date'] = pd.to_datetime(calendar['end_date'], format='%Y%m%d')
+    if (Path(input_folder) / "calendar_dates.txt").exists():
+        calendar_dates = pd.read_csv(
+            os.path.join(input_folder, "calendar_dates.txt"), dtype={"date": str}
+        )
+        calendar_dates['date'] = pd.to_datetime(calendar_dates['date'], format='%Y%m%d')
+    else:
+        calendar_dates = None
+
+    trips = filter_trips_by_date(
+        trips,
+        calendar,
+        pd.to_datetime(departure_date, format='%Y%m%d'),
+        calendar_dates
+    )
 
     # Read stop times
     logger.debug("Read Stop Times")
@@ -283,7 +266,6 @@ def gtfs_to_pyraptor2_timetable(
         trip = Trip()
         trip.hint = trip_row.route_short_name
         trip.route_type = trip_row.route_type
-        # trip.long_name = trip_row.trip_long_name  # e.g., Sprinter
 
         # Iterate over stops
         sort_stop_times = sorted(
@@ -346,10 +328,4 @@ def gtfs_to_pyraptor2_timetable(
 
 if __name__ == "__main__":
     args = parse_arguments()
-    # import shlex
-    # args = parse_arguments(
-    #     shlex.split(r'-i "D:\disser\raptor_timetables\November_2022_2" '
-    #                 r'-o "D:\disser\raptor_timetables\November_2022_2" '
-    #                 r'-d "20221104"')
-    # )
     main(args.input, args.output, args.date)
